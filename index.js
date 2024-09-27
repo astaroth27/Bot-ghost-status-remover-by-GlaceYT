6f1231109fd0fedbd083cd90e4867e8e7987725b
astaroth27 <74060045+astaroth27@users.noreply.github.com>
2024-09-27 18:14:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify"];
+const statusMessages = ["Listening to Spotify","Listening to SoundCloud"];
 
 
 let currentIndex = 0;
